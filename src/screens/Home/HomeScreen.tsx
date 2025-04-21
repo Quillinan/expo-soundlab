82ec1890e0a1b71601c76206145f4e869ee06980
Quillinan <milton.quillinan@gmail.com>
2025-04-21 04:49:40 -0300

    feat: add home

---
+++ src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao projeto de áudio! 🎙️</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
